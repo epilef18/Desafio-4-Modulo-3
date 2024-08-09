e9770a2ad1094fbebae7d2e484ebb5775eaa02b9
epilef18 <f.villasanles@gmail.com>
2024-08-09 18:34:26 -0400

    tercer commit, primera actividad terminada

--- Actividad2/word_count.py
+++ Actividad2/word_count.py
@@ -1,6 +1,13 @@
-with open("Actividad2\lorem_ipsum.txt", "r") as file:
+import sys
+#leer texto externo
+with open("Actividad2\lorem_ipsum.txt", "r") as file:  
     texto=file.read()
 
-    
+#conseguir número de carácteres
+caracteres = len(set(texto))  #set() no permite elementos duplicados, len() da la cantidad de elementos sin duplicar
 
-print(texto)
+palabras = texto.split()
+n_palabras = len(set(palabras))
+
+print(f'El número de carácteres distintos es: {caracteres}')
+print(f'El número de palabras distintas es: {n_palabras}')
